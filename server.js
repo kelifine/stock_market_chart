c1cbaf82d6c555517add04fdc72c14e88f28cf23
Keli Fine <kelfin373@gmail.com>
2017-07-03 15:21:31 +0000

    fixed server.js

--- server.js
+++ server.js
@@ -6,16 +6,12 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 
 var app = express();
-require('dotenv').load();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var routes = require('./app/routes/index.js')(io, app);
 
-mongoose.connect(process.env.MONGO_URI);
-mongoose.Promise = global.Promise;
 
 app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
 
 
 var port = process.env.PORT || 8080;
